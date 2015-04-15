da82ad9876e84e4bae029f52076e34f6a310b652
Brendan Rocks <rocks.brendan@gmail.com>
2015-04-15 22:11:41 +0000

    version 0.2.5

---
+++ R/boxr__internal_misc.R
@@ -0,0 +1,186 @@
+# Short function to tidy up the variable which stores the creation of new remote
+# directories, putting the id at the same place on each
+dir_id_tidy <- function(x){
+  
+  x <- as.character(x)
+  
+  before <- unlist(lapply(strsplit(x, "\\(id: "), function(x) x[1]))
+  
+  after  <- 
+    paste("(id:", unlist(lapply(strsplit(x, "\\(id: "), function(x) x[2])))
+  
+  spaces <- 
+    lapply(
+      nchar(before), 
+      function(y) 
+        paste(rep(" ", max(nchar(before)) - y), collapse = "")
+    )
+  
+  paste0(before, spaces, after)
+}
+
+
+# A simple wrapper for box_auth, with defaul options suitable for running 
+# at startup
+boxAuthOnAttach <- function(){
+  if(Sys.getenv("BOX_AUTH_ON_ATTACH") == "TRUE")
+    try(
+      box_auth(
+        cache = Sys.getenv("BOX_TOKEN_CACHE"),
+        interactive = FALSE, 
+        write.Renv = FALSE
+      ),
+      silent = TRUE
+    )
+}
+
+# A version of cat which only works if the package options are set to verbose,
+# and pads out the message with spaces so that it fills/wipes the console.
+# It also appends \r to the start of each message, so that you can stick them in
+# a loop, for example
+catif <- function(...){
+  if(getOption("boxr.verbose")){
+    txt <- paste(..., collapse = " ")
+    width <- max(getOption("width"), nchar(txt))
+    
+    cat(
+      paste0(
+        "\r", txt, 
+        paste(rep(" ", max(0, width - nchar(txt) - 1)), collapse = "")
+      )
+    )
+  }
+}
+
+# A function to convert the datetime strings that the box api uses, to something
+# R can understand
+box_datetime <- function(x){
+  # R has trouble figuring out the time format
+  x <- as.character(x)
+  # Split out the date/time part
+  dt <- substr(x, 1, nchar(x) - 6)
+  # and the timezone offset
+  tz <- substr(x, nchar(x) - 5, nchar(x))
+  
+  tz <- gsub(":", "", tz)
+  
+  # Note, the timzeone of the datetime boject will be the system default,
+  # bit it's value will have been adjusted to account for the timzone of x
+  as.POSIXct(paste0(dt, tz), format = "%Y-%m-%dT%H:%M:%S%z")
+}
+
+
+checkAuth <- function(){
+  if(is.null(getOption("boxr.token")))
+    stop("It doesn't look like you've set up authentication for boxr yet.\n",
+         "See ?box_auth")
+}
+
+# Something for keeping dir strings a constant length for calls to cat
+trimDir <- function(x, limit = 25){
+  n <- nchar(x)
+  if(n > limit)
+    return(paste0("...", substr(x, n - limit + 3, n)))
+  
+  if(n < limit)
+    return(paste0(paste(rep(" ", limit - n), collapse = ""), x)) else x
+}
+
+
+# For testing -------------------------------------------------------------
+
+# Yoinked from the dev build of testthat
+# https://github.com/hadley/testthat/blob/0835a9e40d3a2fbaac47cbe8f86239e231623b51/R/utils.r
+skip_on_travis <- function() {
+  if (!identical(Sys.getenv("TRAVIS"), "true")) return()
+  
+  testthat::skip("On Travis")
+}
+
+# A function to create a directory structure for testing
+create_test_dir <- function(){
+  # Clear out anything that might already be there
+  unlink("test_dir", recursive = TRUE, force = TRUE)
+  
+  # Set up a test directory structure
+  lapply(
+    c("test_dir/dir_11", "test_dir/dir_12/dir_121/dir_1211", "test_dir/dir_13"),
+    function(x) dir.create(x, recursive = TRUE)
+  )
+  
+  # Create a test file
+  writeLines("This is a test file.", "test_dir/testfile.txt")
+  
+  # Copy the test file into a few of the directories, deliberately leaving some
+  # blank
+  lapply(
+    paste0(list.dirs("test_dir", recursive = TRUE)[-5], "/testfile.txt"),
+    function(x) file.copy("test_dir/testfile.txt", x)
+  )
+  
+  return()  
+}
+
+# A function to modify that directory structure
+modify_test_dir <- function(){
+  # Delete a directory
+  unlink("test_dir/dir_13", recursive = TRUE, force = TRUE)
+  # Add a new directory
+  dir.create("test_dir/dir_14")
+  # Update a file
+  writeLines("This is an updated file", "test_dir/testfile.txt")
+  # Add a file
+  writeLines("This is an new file", "test_dir/newtestfile.txt")
+  # Delete a file  
+  unlink("test_dir/dir_12/testfile.txt")
+  
+  return()
+}
+
+# A function to clear out a box.com directory
+clear_box_dir <- function(dir_id){
+  dir.create("delete_me", showWarnings = FALSE)
+  box_push(dir_id, "delete_me", delete = TRUE)
+  unlink("delete_me", recursive = TRUE, force = TRUE)
+}
+
+modify_remote_dir <- function()
+  suppressMessages({
+      tf1 <- 
+        normalizePath(paste0(tempdir(), "/testfile.txt"), mustWork = FALSE)
+      
+      tf2 <- 
+        normalizePath(paste0(tempdir(), "/newtestfile.txt"), mustWork = FALSE)
+      
+      writeLines("This text is NEW!", tf1)
+      writeLines("This text is NEW!", tf2)
+      
+      bls <- box_ls(0)
+      
+      # Upload a new file
+      # test_dir/newtestfile.txt
+      box_ul(0, tf2)
+      
+      # Update an existing file: 
+      # test_dir/dir_11/testfile.txt
+      box_ul(bls$id[bls$name == "dir_12"], tf1)
+      
+      # Create a new dir, and put a new file in it
+      # test_dir/another_dir/newtestfile.txt
+      new_dir <- box_dir_create("another_dir", 0)
+      box_ul(httr::content(new_dir)$id, tf2)
+      
+      # Delete a file
+      # test_dir/testfile.txt
+      box_delete_file(bls$id[bls$name == "testfile.txt"])
+      
+      # Delete a a folder (it has a file in it)
+      box_delete_folder(bls$id[bls$name == "dir_11"])
+      
+    })
+
+
+#' @keywords internal
+forRCMDCheck <- function(cran = "http://cran.r-project.org/"){
+  httpuv::encodeURI(cran)
+}
---
+++ R/boxr_s3_classes.R
@@ -0,0 +1,221 @@
+
+# File References ---------------------------------------------------------
+
+
+#' @export
+print.boxr_file_reference <- function(x, ...){
+  ob <- x$entries[[1]]
+  cat("box.com file reference (package: boxr)\n\n")
+  cat(" name        :", ob$name, "\n")
+  cat(" file id     :", ob$id, "\n")
+  cat(" version     :", paste0("V", as.numeric(ob$etag) + 1), "\n")
+  cat(" size        :", format_bytes(ob$size), "\n")
+  cat(" modified at :", 
+      as.character(as.POSIXct(gsub("T", " ", ob$modified_at))), "\n"
+  )
+  cat(" created at  :", 
+      as.character(as.POSIXct(gsub("T", " ", ob$modified_at))), "\n"
+  )
+  cat(" uploaded by :", ob$modified_by$login, "\n")
+  cat(" owned by    :", ob$owned_by$login, "\n")
+  shared_link <- ob$shared_link
+  if(is.null(shared_link))
+    shared_link <- "None"
+  cat(" shared link :", shared_link, "\n\n")
+  cat(" parent folder name : ", ob$parent$name, "\n")
+  cat(" parent folder id   : ", ob$parent$id, "\n")
+  
+  invisible(x)
+}
+
+
+
+
+# Directory-Wide Operations -----------------------------------------------
+
+
+# A better version of this would keep the whole httr call, in additon
+# to the boxr expression called (e.g. upload call : box_ul(blah))
+#' @export
+print.boxr_dir_wide_operation_result <- function(x, ...){
+  
+  boxr_timediff <- function(x)
+    paste0("took ", format(unclass(x), digits = 3), " ", attr(x, "units"))
+  
+  f <- x$file_list
+  
+  tdif <- boxr_timediff(x$end - x$start)
+  
+  cat("boxr", x$operation, "operation\n\n")
+  
+  # General blurb on the op
+  cat(paste0(
+    "  User           : ", getOption("boxr.username"), "\n",
+    "  Local dir      : ", x$local_tld, "\n",
+    "  box.com folder : ", x$box_tld_id, "\n",
+    "  started at     : ", x$start , " (", tdif, ")", "\n",
+    "\n"
+  ))
+  
+  # Produce a summary of the changes
+  summary_items <- 
+    unlist(mapply(
+      function(x, msg) if(nrow(x) > 0L) paste(nrow(x), msg), 
+      x$file_list, x$msg_list
+    ))
+  
+  cat(paste0(
+    paste(summary_items[!is.null(summary_items)], collapse = ", "), 
+    ".\n\n"
+  ))
+  
+  cat("Use summary() to see individual file operations.")
+  invisible(x)
+}
+
+
+
+# This will only really be shown for uploaded files. I can't think of a great
+# reason to explicitly 'map' this to local versions of a file at the moment.
+#
+# A better version of this would keep the whole httr call, in additon
+# to the boxr expression called (e.g. upload call : box_ul(blah))
+#' @export
+summary.boxr_dir_wide_operation_result <- function(object, ...){
+  
+  boxr_timediff <- function(x)
+    paste0("took ", format(unclass(x), digits = 3), " ", attr(x, "units"))
+  
+  f <- object$file_list
+  
+  tdif <- boxr_timediff(object$end - object$start)
+  
+  cat("boxr", object$operation, "operation\n\n")
+  
+  # General blurb on the op
+  cat(paste0(
+    "  User           : ", getOption("boxr.username"), "\n",
+    "  Local dir      : ", object$local_tld, "\n",
+    "  box.com folder : ", object$box_tld_id, "\n",
+    "  started at     : ", object$start , " (", tdif, ")", "\n",
+    "\n"
+  ))
+  
+  # This just justifies the box.com id's
+  if(!is.null(object$file_list[[17]]) && nrow(object$file_list[[17]]) > 0)
+    object$file_list[[17]][,1] <- dir_id_tidy(object$file_list[[17]][,1])
+  
+  print_df <- function(x, msg){
+    if(nrow(x) > 0){
+      cat(nrow(x), msg, ":\n")
+      print(
+        format(
+          data.frame(
+            " " = x[,grepl("full_path",colnames(x))],
+            check.names = FALSE
+          ), 
+          justify = "left"
+        ), 
+        row.names = FALSE
+      )
+      cat("\n\n")
+    }
+  }
+  
+  # Run through the file df's in file_list, print out messages for them
+  dummy_var <- mapply(print_df, object$file_list, object$msg_list)
+  
+  invisible(object)
+}
+
+
+
+# Directory Comparison ----------------------------------------------------
+
+
+#' @export
+print.boxr_dir_comparison <- function(x, ...){
+  cat("boxr remote:local directory comparison\n\n")
+  
+  origin <- if(x$call_info$load == "up") "Local directory" else "box.com"
+  destin <- if(x$call_info$load != "up") "Local directory" else "box.com" 
+  
+  # General blurb on the op
+  cat(paste0(
+    "  User           : ", getOption("boxr.username"),    "\n",
+    "  Local dir      : ", x$call_info$local_dir,    "\n",
+    "  box.com folder : ", x$call_info$dir_id,       "\n",
+    "  Direction      : ", x$call_info$load, "load", "\n",
+    "  Origin         : ", origin,                        "\n",
+    "  Destination    : ", destin,                        "\n",
+    "\n"
+  ))
+  
+  object_list <- x[names(x) != "call_info"]
+  
+  # Produce a summary of the differences
+  summary_items <- 
+    unlist(mapply(
+      function(x, msg) if(nrow(x) > 0L) paste(nrow(x), msg), 
+      object_list, x$call_info$msg
+    ))
+  
+  cat(paste0(
+    paste(summary_items[!is.null(summary_items)], collapse = ", "), 
+    ".\n\n"
+  ))
+  #   
+  cat("Use summary() to see individual files.")
+  
+  invisible(x)
+  
+}
+
+
+#' @export
+summary.boxr_dir_comparison <- function(object, ...){
+  cat("boxr remote:local directory comparison\n\n")
+  
+  origin <- if(object$call_info$load == "up") "Local directory" else "box.com"
+  destin <- if(object$call_info$load != "up") "Local directory" else "box.com" 
+  
+  # General blurb on the op
+  cat(paste0(
+    "  User           : ", getOption("boxr.username"),    "\n",
+    "  Local dir      : ", object$call_info$local_dir,    "\n",
+    "  box.com folder : ", object$call_info$dir_id,       "\n",
+    "  Direction      : ", object$call_info$load, "load", "\n",
+    "  Origin         : ", origin,                        "\n",
+    "  Destination    : ", destin,                        "\n",
+    "\n"
+  ))
+  
+  object_list <- object[names(object) != "call_info"]
+  
+  # This just justifies the box.com id's
+  if(!is.null(object$file_list[[17]]) && nrow(object$file_list[[17]]) > 0)
+    object$file_list[[17]][,1] <- dir_id_tidy(object$file_list[[17]][,1])
+  
+  print_df <- function(x, msg){
+    if(nrow(x) > 0){
+      cat(nrow(x), msg, ":\n")
+      print(
+        format(
+          data.frame(
+            " " = x[,grepl("full_path",colnames(x))],
+            check.names = FALSE
+          ), 
+          justify = "left"
+        ), 
+        row.names = FALSE
+      )
+      cat("\n\n")
+    }
+  }
+  
+  # Run through the file df's in file_list, print out messages for them
+  dummy_var <- mapply(print_df, object_list, object$call_info$msg)
+  
+  invisible(object)
+  
+}
